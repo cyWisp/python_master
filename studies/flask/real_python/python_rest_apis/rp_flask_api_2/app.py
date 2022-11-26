260ce486f81389d582036eee8459bbef20f29426
cywisp <cywisp@gmail.com>
2022-11-26 14:27:55 -0500

    flask api study

--- studies/flask/real_python/python_rest_apis/rp_flask_api_2/app.py
+++ studies/flask/real_python/python_rest_apis/rp_flask_api_2/app.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+@app.route('/index/')
+def index():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8084, debug=True)
