4d1c519f4416d7987ec1008aa0ff0153ab4def0a
cywisp <cywisp@gmail.com>
2023-08-18 16:57:15 -0400

    Added django and bootstrap work

--- studies/django/django_for_beginners/hello_world/pages/urls.py
+++ studies/django/django_for_beginners/hello_world/pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home_page_view
+from .views import homePageView
 
 
 urlpatterns = [
-    path('', home_page_view, name='home')
+    path('', homePageView, name='home')
 ]
