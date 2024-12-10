19e871a637b50a2439d471c694017365a5ece28f
cywisp <cywisp@gmail.com>
2024-12-10 09:24:25 -0500

    Added a script to seed rules for portal

--- playground/seed_rules/seed.py
+++ playground/seed_rules/seed.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjkwNCwib2t0YUlkIjoicm9iZXJ0by5kYWdsaW9AZXNpbXBsaWNpdHkuY29tIiwiaGFycElkIjoicm9iZXJ0by5kYWdsaW9AZXNpbXBsaWNpdHkuY29tIiwiZmlyc3ROYW1lIjoiUm9iIiwibGFzdE5hbWUiOiJEYWdsaW8iLCJlbWFpbCI6InJvYmVydG8uZGFnbGlvQGVzaW1wbGljaXR5LmNvbSIsInJvbGVzIjpbeyJvcmdhbml6YXRpb25fY29udHJhY3RfaWQiOjEyLCJkYXRhX2FjY2Vzc19ncm91cCI6ImNkci1wZGEtcWRhcyIsInJvbGVfaWQiOjQsInJvbGVfbmFtZSI6ImV4dGVybmFsYWRtaW4iLCJyb2xlX2Rlc2NyaXB0aW9uIjpudWxsLCJkYXRlX2NyZWF0ZWQiOiIyMDI0LTEyLTA2VDE2OjA1OjA2LjkyM1oifSx7Im9yZ2FuaXphdGlvbl9jb250cmFjdF9pZCI6MTIsImRhdGFfYWNjZXNzX2dyb3VwIjoiY2RyLXBkYS1xZGFzIiwicm9sZV9pZCI6Miwicm9sZV9uYW1lIjoiZHVhb3duZXIiLCJyb2xlX2Rlc2NyaXB0aW9uIjpudWxsLCJkYXRlX2NyZWF0ZWQiOiIyMDI0LTEyLTA2VDE2OjA0OjUzLjYyMFoifV0sImlhdCI6MTczMzUwMTExOSwiZXhwIjoxNzMzNTQ0MzE5fQ.9YnOVAWFhMwjemO8Z0j3-YBX9l73HI7Vu5WscnEwiws'
+BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjkwNCwib2t0YUlkIjoicm9iZXJ0by5kYWdsaW9AZXNpbXBsaWNpdHkuY29tIiwiaGFycElkIjoicm9iZXJ0by5kYWdsaW9AZXNpbXBsaWNpdHkuY29tIiwiZmlyc3ROYW1lIjoiUm9iIiwibGFzdE5hbWUiOiJEYWdsaW8iLCJlbWFpbCI6InJvYmVydG8uZGFnbGlvQGVzaW1wbGljaXR5LmNvbSIsInJvbGVzIjpbeyJvcmdhbml6YXRpb25fY29udHJhY3RfaWQiOjEyLCJkYXRhX2FjY2Vzc19ncm91cCI6ImNkci1wZGEtcWRhcyIsInJvbGVfaWQiOjIsInJvbGVfbmFtZSI6ImR1YW93bmVyIiwicm9sZV9kZXNjcmlwdGlvbiI6bnVsbCwiZGF0ZV9jcmVhdGVkIjoiMjAyNC0xMi0wNlQxNjowNDo1My42MjBaIn0seyJvcmdhbml6YXRpb25fY29udHJhY3RfaWQiOjEyLCJkYXRhX2FjY2Vzc19ncm91cCI6ImNkci1wZGEtcWRhcyIsInJvbGVfaWQiOjQsInJvbGVfbmFtZSI6ImV4dGVybmFsYWRtaW4iLCJyb2xlX2Rlc2NyaXB0aW9uIjpudWxsLCJkYXRlX2NyZWF0ZWQiOiIyMDI0LTEyLTA2VDE2OjA1OjA2LjkyM1oifV0sImlhdCI6MTczMzc2MDY4NCwiZXhwIjoxNzMzODAzODg0fQ.RaSRcO16XH9WQXksHApZhnXbUeZ26IEY1UAWth80J7k'
 
 URL = 'http://localhost:3003/api/organizations/'
 
@@ -22,10 +22,10 @@ if __name__ == '__main__':
 
 
 
-    for i in range(5000):
+    for i in range(10000):
         body = {
-          "name": f"python-org-{i}",
-          "organizationName": f"python-org-{i}",
+          "name": f"python-org-{i + 6000}",
+          "organizationName": f"python-org-{i + 6000}",
           "iamRole": "Dua Owner"
         }
 
